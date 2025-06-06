6d976a3b428536a8b9658ff2e95d7dc885eda10e
pietzfistlucas <137784084+pietzfistlucas@users.noreply.github.com>
2025-06-06 09:01:15 -0300

    ok

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const botoes =document.querySelectorAll(".botao");
-const botoes =document.querySelectorAll(".aba-conteudo");
+const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0;<botoes.length; j++){
 botoes[i].onclick = function () {
@@ -11,6 +11,26 @@ botoes[i].onclick = function () {
 
     botoes[i].classList.add("ativo");
     texto[j].classList.remove("ativo");
-}
+  }
 }
 
+  const contadores = document.querySelectorAll(".contador");
+  const tempoObjetivo1 = new Date("2028-12-30T00:00:00");
+
+
+  contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+  function calculaTempo(tempoObjetivo); {
+    let tempoAtual = new date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor (segundos / 60);
+    let horas = Math.floor (horas / 60);
+    let dias = Math.floor (dias / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    retun dias + " dias " + " horas " + " horas " + " minutos " + " minutos " + " segundos " + " segundos ";
+  }
